b4e6bd58a35891f818ff19c45c7a889d205c5d7e
yasmeenokh <ykhatib44@gmail.com>
2021-07-23 17:49:52 +0300

    other's request Details

--- peerpair/src/pages/userRequest.jsx
+++ peerpair/src/pages/userRequest.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import OwnerRequestDetails from '../../src/components/ownerRequestDetails/ownerRequestDetails.jsx';
 import RequestSubmitters from '../../src/components/submitters/submitters.jsx';
-
+import OtherRequestDetails from '../../src/components/otherRequestDetails/otherRequestDetails';
 
 //for route '/request/id' 
 const UserRequestPage = (props) =>{
@@ -9,6 +9,7 @@ const UserRequestPage = (props) =>{
         <>
             <OwnerRequestDetails/>
             <RequestSubmitters/>
+            <OtherRequestDetails/>
         </>
     )
 }
